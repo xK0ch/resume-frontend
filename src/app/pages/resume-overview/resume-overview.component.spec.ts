85edd7414e9c6228000ba0099eda3e7e631617d2
xK0ch <mail@fynn-koch.de>
2024-10-03 12:09:09 +0200

    Add animation to resume overview

--- src/app/pages/resume-overview/resume-overview.component.spec.ts
+++ src/app/pages/resume-overview/resume-overview.component.spec.ts
@@ -4,6 +4,7 @@ import {
   ComponentFixture,
   TestBed,
 } from '@angular/core/testing';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { ResumeOverviewComponent } from './resume-overview.component';
 
@@ -17,6 +18,7 @@ describe('ResumeOverviewComponent', () => {
       providers: [
         provideHttpClient(),
         provideHttpClientTesting(),
+        provideAnimationsAsync(),
       ],
     })
       .compileComponents();
--- src/app/pages/resume-overview/resume-overview.component.ts
+++ src/app/pages/resume-overview/resume-overview.component.ts
@@ -1,4 +1,10 @@
 import {
+  animate,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import {
   ChangeDetectionStrategy,
   Component,
   inject,
@@ -25,10 +31,47 @@ import { GeneralInformationComponent } from './general-information/general-infor
   ],
   templateUrl: './resume-overview.component.html',
   styleUrl: './resume-overview.component.scss',
+  animations: [
+    trigger('slideInFromTop', [
+      transition(':enter', [
+        style({
+          transform: 'translateY(-100%)',
+          opacity: 0,
+        }),
+        animate('1.5s ease-out', style({
+          transform: 'translateY(0)',
+          opacity: 1,
+        })),
+      ]),
+    ]),
+    trigger('slideInFromBottom', [
+      transition(':enter', [
+        style({
+          transform: 'translateY(100%)',
+          opacity: 0,
+        }),
+        animate('1.5s ease-out', style({
+          transform: 'translateY(0)',
+          opacity: 1,
+        })),
+      ]),
+    ]),
+    trigger('slideInFromLeft', [
+      transition(':enter', [
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0,
+        }),
+        animate('1.5s ease-out', style({
+          transform: 'translateX(0)',
+          opacity: 1,
+        })),
+      ]),
+    ]),
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ResumeOverviewComponent {
 
   resumeStore = inject(ResumeStore);
-
 }
